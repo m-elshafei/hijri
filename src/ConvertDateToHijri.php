0c05d9a28b8240bee235fd55fe0754ead1db8713
Moustafa Elshafei <moustafaelshafhy@gmail.com>
2024-09-02 07:14:50 +0300

    add

---
+++ src/ConvertDateToHijri.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace MoustafaElshafei\ConvertDateToHijri;
+
+use InvalidArgumentException;
+use Illuminate\Console\Command;
+use Illuminate\Support\Str;
+
+class ConvertDateToHijri 
+{
+    public function index()
+    {
+        return 'Hello';
+    }
+
+}
